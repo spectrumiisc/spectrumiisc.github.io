08a3ff8be731da273d51c8679c02f9cda1645c1e
eclipseiiscug <93331044+eclipseiiscug@users.noreply.github.com>
2021-10-31 01:02:44 +0530

    Update neuroel_main.js

--- static/scripts/neuroel_main.js
+++ static/scripts/neuroel_main.js
@@ -94,13 +94,15 @@ function processData(allText) {
 	var team_data = [];
 	var sum = 0;
 	for (var i=0; i<lines.length;i++){
-		while (ind == lines[i][0] && i<lines.length){
+				while (i<lines.length && ind == lines[i][0]){
 			sum += parseInt(lines[i][1]);
 			i++;
 		} 
-		team_data.push([ind,sum]);
-		ind = lines[i][0];
-		sum = parseInt(lines[i][1]);
+		if (i<lines.length){
+			team_data.push([ind,sum]);
+			ind = lines[i][0];
+			sum = parseInt(lines[i][1]);
+		}
 	}
 	team_data.push([ind,sum]);
 	team_data.sort(sortFunction2);
